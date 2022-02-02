3fa7fced914c7b3434acc459bbec1f1047c702d2
fryles <mylesmarr@gmail.com>
2022-02-01 19:21:07 -0800

    added more images

--- public/resources/makewebp.sh
+++ public/resources/makewebp.sh
@@ -1,4 +1,5 @@
-for file in pngs/*.png
+for file in pngs/*
 do
-cwebp -q 80 -resize 500 500 -alpha_q 100 "$file" -o "/${file%.png}.webp"
+cwebp -q 80 -resize 500 500 -alpha_q 100 "$file" -o "./${file%.png}.webp"
+mv "$file" ./
 done
